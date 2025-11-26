7929cca24b6852cfafadc1a3aa9030d686f79e56
Evan Alvarez <evanfalvarez@tutamail.com>
2025-11-26 01:03:38 -0700

    LETS ALL LOVE LAIN

--- init.lua
+++ init.lua
@@ -4,37 +4,7 @@ require("evan.lsp")
 
 vim.cmd.packadd 'rustaceanvim'
 
--- load plugin files (stolen from miroshQa/dotfiles)
-
-local function load_file(path)
-    local co = coroutine.running()
-    vim.defer_fn(function()
-        -- local start = vim.uv.hrtime()
-        loadfile(path)()
-        -- print(path, " loading time: ", (vim.uv.hrtime() - start) / 1000000, "ms")
-        coroutine.resume(co)
-    end, 2)
-    coroutine.yield()
+-- load plugin files
+for _, path in ipairs(vim.api.nvim_get_runtime_file("lua/evan/plugins/*.lua", true)) do
+    dofile(path)
 end
-
-coroutine.wrap(function()
-    local groups = {
-        vim.api.nvim_get_runtime_file("lua/evan/plugins/*.lua", true),
-    }
-    for _, files in ipairs(groups) do
-        for _, path in ipairs(files) do
-            load_file(path)
-        end
-    end
-
-    local cur_ft = vim.bo.filetype
-    for ft, callback in pairs(vim.ftplugin) do
-        if ft == cur_ft then
-            callback()
-        end
-        vim.api.nvim_create_autocmd("FileType", {
-            pattern = ft,
-            callback = callback
-        })
-    end
-end)()
